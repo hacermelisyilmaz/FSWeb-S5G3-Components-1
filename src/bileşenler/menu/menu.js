687e2e59a27d3fcf4efaf38e940fb1683d09422f
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-07-26 15:23:43 +0300

    Step 1 is completed: The function 'menuYapici' is created which creates a div and ul

--- src/bileşenler/menu/menu.js
+++ src/bileşenler/menu/menu.js
@@ -25,7 +25,7 @@ let menuElemanlari = [
   Tüm bu öğeleri <ul>'a ekleyin
 
   Adım 3: Hala fonksiyon içindeyiz, DOM'dan menü düğmesini seçin (`index.html` içinde `menu-button` sınıfına (class) sahip öğe).
-
+  
   Adım 4: Menü butonuna bir `click` event dinleyicisi ekleyin. Butona her tıklanıldığında div.menu classına `menu--open`('menu' class'ına sahip olan div) ekleyip/silecek(toogle).
   
   İPUCU: Javascript'te toogle metotları bir öğeyi bir elemana ekleyip/çıkarmaya yarar. Örnek olarak bir öğeye tıklandığında bir class toogle'ı kullanıldığında, o öğreye tıklandığında eğer o class ismi öğede yoksa ekler, eğer o class ismi öğede varsa siler. Toogle - aç kapa manasına da gelir.
@@ -34,3 +34,10 @@ let menuElemanlari = [
 
   Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
 */
+
+function menuYapici(aMenuElemanlari) {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  const liste = document.createElement("ul");
+}
